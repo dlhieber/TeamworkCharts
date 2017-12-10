5e8e4b088ab8d9f1dd70cabe3683acbb9022f4a7
DESKTOP-3HTC6SJ\dan_h <dan_hiebert@ymail.com>
2017-12-10 16:22:33 -0700

    Displaying data

--- src/OptionWindow.java
+++ src/OptionWindow.java
@@ -7,6 +7,7 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.text.MaskFormatter;
 
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.awt.GridBagLayout;
 import javax.swing.JLabel;
@@ -18,6 +19,7 @@ import java.awt.Toolkit;
 import java.awt.event.ActionListener;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.awt.event.ActionEvent;
@@ -26,6 +28,7 @@ import java.awt.Button;
 import java.awt.Color;
 import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
+import javax.swing.JComboBox;
 
 public class OptionWindow extends JFrame {
 
@@ -48,10 +51,15 @@ public class OptionWindow extends JFrame {
 		});
 	}
 
+	private Object makeObj(final String item)  {
+	     return new Object() { public String toString() { return item; } };
+	   }
 	/**
 	 * Create the frame.
 	 * @throws ParseException 
 	 */
+	
+	
 	public OptionWindow() throws ParseException {
 		setIconImage(Toolkit.getDefaultToolkit().getImage(OptionWindow.class.getResource("/Logo-2016-Swirl-125.png")));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -60,12 +68,14 @@ public class OptionWindow extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{116, 75, 53, 50, 168};
+		gbl_contentPane.columnWidths = new int[]{116, 75, 0, 53, 50, 168};
 		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gbl_contentPane.columnWeights = new double[]{0.0, 1.0, 1.0, 0.0, 0.0};
+		gbl_contentPane.columnWeights = new double[]{0.0, 1.0, 0.0, 1.0, 0.0, 1.0};
 		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		contentPane.setLayout(gbl_contentPane);
+		JButton btnShowChart = new JButton("Show Chart");
 		
+		btnShowChart.setVisible(false);
 		JLabel lblEnterStartDate = new JLabel("Start Date (dd/mm/yyyy):");
 		GridBagConstraints gbc_lblEnterStartDate = new GridBagConstraints();
 		gbc_lblEnterStartDate.insets = new Insets(0, 0, 5, 5);
@@ -87,7 +97,7 @@ public class OptionWindow extends JFrame {
 		GridBagConstraints gbc_btnSet = new GridBagConstraints();
 		gbc_btnSet.gridwidth = 2;
 		gbc_btnSet.insets = new Insets(0, 0, 5, 0);
-		gbc_btnSet.gridx = 3;
+		gbc_btnSet.gridx = 4;
 		gbc_btnSet.gridy = 0;
 		contentPane.add(btnSet, gbc_btnSet);
 		
@@ -99,7 +109,7 @@ public class OptionWindow extends JFrame {
 		contentPane.add(lblNumberOfWeeks, gbc_lblNumberOfWeeks);
 		
 		JSpinner spinner = new JSpinner();
-		spinner.setModel(new SpinnerNumberModel(1, 1, 104, 1));
+		spinner.setModel(new SpinnerNumberModel(12, 1, 104, 1));
 		GridBagConstraints gbc_spinner = new GridBagConstraints();
 		gbc_spinner.insets = new Insets(0, 0, 5, 5);
 		gbc_spinner.gridx = 1;
@@ -110,27 +120,35 @@ public class OptionWindow extends JFrame {
 		GridBagConstraints gbc_lblNotValid = new GridBagConstraints();
 		gbc_lblNotValid.gridwidth = 2;
 		gbc_lblNotValid.insets = new Insets(0, 0, 5, 0);
-		gbc_lblNotValid.gridx = 3;
+		gbc_lblNotValid.gridx = 4;
 		gbc_lblNotValid.gridy = 1;
 		contentPane.add(lblNotValid, gbc_lblNotValid);
 		lblNotValid.setVisible(false);
-		contentPane.add(lblNewLabel, gbc_lblNewLabel);
-		
-		JLabel lblStartDateNot = new JLabel("Start Date Not Set!");
-		lblStartDateNot.setForeground(Color.RED);
-		GridBagConstraints gbc_lblStartDateNot = new GridBagConstraints();
-		gbc_lblStartDateNot.gridwidth = 2;
-		gbc_lblStartDateNot.insets = new Insets(0, 0, 5, 0);
-		gbc_lblStartDateNot.gridx = 3;
-		gbc_lblStartDateNot.gridy = 7;
-		lblStartDateNot.setVisible(false);
-		contentPane.add(lblStartDateNot, gbc_lblStartDateNot);
+	
+		JComboBox<String> comboBox = new JComboBox<String>();
+		GridBagConstraints gbc_comboBox = new GridBagConstraints();
+		gbc_comboBox.insets = new Insets(0, 0, 5, 0);
+		gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
+		gbc_comboBox.gridx = 5;
+		gbc_comboBox.gridy = 2;
+		contentPane.add(comboBox, gbc_comboBox);
+		btnShowChart.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println(comboBox.getItemAt(comboBox.getSelectedIndex()));
+				charts.showChart(comboBox.getItemAt(comboBox.getSelectedIndex()));
+				
+			}
+		});
 		JButton btnLoadOldData = new JButton("Load Old Data");
 		btnLoadOldData.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {if(charts.isStartCalendarSet()){
 						charts.getOldData((Integer)spinner.getValue());
-						
+						ArrayList<String> people=charts.getListOfPeople();
+						for(int i=0;i<people.size();i++){
+							comboBox.addItem(people.get(i));							
+						}
+						btnShowChart.setVisible(true);
 					}else{
 						lblNewLabel.setVisible(true);
 					}
@@ -141,20 +159,23 @@ public class OptionWindow extends JFrame {
 			}
 		});
 		
-		JFormattedTextField frmtdtxtfldDdmmyyyy = new JFormattedTextField(new MaskFormatter("##/##/####"));
-		frmtdtxtfldDdmmyyyy.setText("dd/mm/yyyy");
-		GridBagConstraints gbc_frmtdtxtfldDdmmyyyy = new GridBagConstraints();
-		gbc_frmtdtxtfldDdmmyyyy.gridwidth = 2;
-		gbc_frmtdtxtfldDdmmyyyy.insets = new Insets(0, 0, 5, 5);
-		gbc_frmtdtxtfldDdmmyyyy.fill = GridBagConstraints.HORIZONTAL;
-		gbc_frmtdtxtfldDdmmyyyy.gridx = 1;
-		gbc_frmtdtxtfldDdmmyyyy.gridy = 0;
-		contentPane.add(frmtdtxtfldDdmmyyyy, gbc_frmtdtxtfldDdmmyyyy);
+		btnLoadOldData.setVisible(false);
+		
+		JLabel lblStartDateNot = new JLabel("Start Date Not Set!");
+		lblStartDateNot.setForeground(Color.RED);
+		GridBagConstraints gbc_lblStartDateNot = new GridBagConstraints();
+		gbc_lblStartDateNot.gridwidth = 2;
+		gbc_lblStartDateNot.insets = new Insets(0, 0, 5, 0);
+		gbc_lblStartDateNot.gridx = 4;
+		gbc_lblStartDateNot.gridy = 7;
+		lblStartDateNot.setVisible(false);
+		contentPane.add(lblStartDateNot, gbc_lblStartDateNot);
+		
 		GridBagConstraints gbc_btnLoadOldData = new GridBagConstraints();
 		gbc_btnLoadOldData.gridwidth = 2;
 		gbc_btnLoadOldData.insets = new Insets(0, 0, 5, 5);
 		gbc_btnLoadOldData.gridx = 0;
-		gbc_btnLoadOldData.gridy = 6;
+		gbc_btnLoadOldData.gridy = 2;
 		contentPane.add(btnLoadOldData, gbc_btnLoadOldData);
 		
 		JButton btnNewButton = new JButton("Load New Data");
@@ -163,6 +184,12 @@ public class OptionWindow extends JFrame {
 				try {
 					if(charts.isStartCalendarSet()){
 						charts.getNewData((Integer)spinner.getValue());
+						
+						ArrayList<String> people=charts.getListOfPeople();
+						for(int i=0;i<people.size();i++){
+							comboBox.addItem(people.get(i));							
+						}
+						btnShowChart.setVisible(true);
 					}else{
 						lblStartDateNot.setVisible(true);
 					}
@@ -172,6 +199,29 @@ public class OptionWindow extends JFrame {
 				}
 			}
 		});
+		
+		btnNewButton.setVisible(false);
+		
+	
+		
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.gridwidth = 2;
+		gbc_btnNewButton.insets = new Insets(0, 0, 5, 5);
+		gbc_btnNewButton.gridx = 0;
+		gbc_btnNewButton.gridy = 3;
+		contentPane.add(btnNewButton, gbc_btnNewButton);
+		contentPane.add(lblNewLabel, gbc_lblNewLabel);
+		
+		
+		JFormattedTextField frmtdtxtfldDdmmyyyy = new JFormattedTextField(new MaskFormatter("##/##/####"));
+		frmtdtxtfldDdmmyyyy.setText("dd/mm/yyyy");
+		GridBagConstraints gbc_frmtdtxtfldDdmmyyyy = new GridBagConstraints();
+		gbc_frmtdtxtfldDdmmyyyy.gridwidth = 3;
+		gbc_frmtdtxtfldDdmmyyyy.insets = new Insets(0, 0, 5, 5);
+		gbc_frmtdtxtfldDdmmyyyy.fill = GridBagConstraints.HORIZONTAL;
+		gbc_frmtdtxtfldDdmmyyyy.gridx = 1;
+		gbc_frmtdtxtfldDdmmyyyy.gridy = 0;
+		contentPane.add(frmtdtxtfldDdmmyyyy, gbc_frmtdtxtfldDdmmyyyy);
 		
 	btnSet.addActionListener(new ActionListener() {
 			
@@ -189,6 +239,8 @@ public class OptionWindow extends JFrame {
 					lblStartDateNot.setVisible(false);
 					lblNewLabel.setVisible(false);
 					lblNotValid.setVisible(true);
+					btnLoadOldData.setVisible(true);
+					btnNewButton.setVisible(true);
 				}catch(Exception e2){
 					lblNotValid.setVisible(true);
 				}
@@ -198,25 +250,12 @@ public class OptionWindow extends JFrame {
 			}
 		});
 		
-		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
-		gbc_btnNewButton.gridwidth = 2;
-		gbc_btnNewButton.insets = new Insets(0, 0, 5, 0);
-		gbc_btnNewButton.gridx = 3;
-		gbc_btnNewButton.gridy = 6;
-		contentPane.add(btnNewButton, gbc_btnNewButton);
-		
-		JButton btnShowChart = new JButton("Show Chart");
-		btnShowChart.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				charts.showChart();
-			}
-		});
 		
 		
 		GridBagConstraints gbc_btnShowChart = new GridBagConstraints();
 		gbc_btnShowChart.anchor = GridBagConstraints.SOUTH;
 		gbc_btnShowChart.fill = GridBagConstraints.HORIZONTAL;
-		gbc_btnShowChart.gridwidth = 5;
+		gbc_btnShowChart.gridwidth = 6;
 		gbc_btnShowChart.gridx = 0;
 		gbc_btnShowChart.gridy = 8;
 		contentPane.add(btnShowChart, gbc_btnShowChart);
--- src/WeeklyCharts.java
+++ src/WeeklyCharts.java
@@ -7,6 +7,8 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.io.FileWriter;
 import java.io.FileReader;
 
@@ -30,7 +32,7 @@ public class WeeklyCharts {
 	static String filename="Teamwork Tasks.txt";
 	static long CheckTime=18000000;
 	static long numOfMilliSecondsinDay=86400000;
-	static JSONArray tasks;
+	JSONArray tasks;
 	static JFrame optionFrame;
 	Calendar startCalendar;
 	boolean dataIsLoaded;
@@ -38,7 +40,9 @@ public class WeeklyCharts {
 	ArrayList<Calendar> weeks;
 	SimpleDateFormat sdf;
 	String weekKey="weekContributions";
-	
+	HashMap<String,Double> Totals;
+	HashSet<String> projects;
+		
 	public void setStartWeek(Calendar start){
 		startCalendar=start;
 		startCalendarSet=true;
@@ -56,7 +60,7 @@ public class WeeklyCharts {
 				
 				newWeek=(Calendar) newWeek.clone();
 				newWeek.add(Calendar.DATE, 7);
-				System.out.println("Added Week: "+ sdf.format(newWeek.getTime()));
+		//		System.out.println("Added Week: "+ sdf.format(newWeek.getTime()));
 		//		System.out.println((newWeek.getTimeInMillis()-weeks.get(0).getTimeInMillis())/(double)numOfMilliSecondsinDay);
 				
 				
@@ -67,7 +71,9 @@ public class WeeklyCharts {
 		}
 	}
 	
-	private double getWeekPorportionInHours(Calendar weekStart,Calendar taskStart, Calendar taskEnd, int totalTimeInMinutes){
+
+	
+	private double getWeekPorportionInHours(Calendar weekStart, Calendar taskStart, Calendar taskEnd, int totalTimeInMinutes){
 		weekStart.set(weekStart.get(Calendar.YEAR), weekStart.get(Calendar.MONTH), weekStart.get(Calendar.DAY_OF_MONTH), 1, 1, 1);
 		taskStart.set(taskStart.get(Calendar.YEAR), taskStart.get(Calendar.MONTH), taskStart.get(Calendar.DAY_OF_MONTH), 1, 1, 1);
 		taskEnd.set(taskEnd.get(Calendar.YEAR), taskEnd.get(Calendar.MONTH), taskEnd.get(Calendar.DAY_OF_MONTH), 1, 1, 1);
@@ -95,17 +101,44 @@ public class WeeklyCharts {
 		return newCalendar;
 	}
 	
+	public ArrayList<String> getListOfPeople() {
+		
+		
+		HashSet<String> people= new HashSet<String>();
+		ArrayList<String> sortedPeople=new ArrayList<String>();
+		for(int i=0;i<tasks.length();i++){
+			
+			try {
+					people.add(tasks.getJSONObject(i).getString("responsible-party-summary"));
+				} catch (JSONException e) {
+					people.add("Anybody");
+				}
+			}			
+		Object[] unsortedListOfPeople= people.toArray();
+		
+		for(int i=0;i<unsortedListOfPeople.length;i++){
+			sortedPeople.add((String) unsortedListOfPeople[i]);
+		}
+		sortedPeople.sort(String::compareToIgnoreCase);
+		
+		return sortedPeople;
+	}
+	
+	
+	
+	
 	private void addTimeCalculationsToTasks(int numOfWeeks) throws JSONException{
 		if(startCalendarSet){
 			setWeeks(numOfWeeks);
 			int size=tasks.length();
 			
 			for (int i=0;i<size;i++){
-				ArrayList<Double> taskContributions=new ArrayList<Double>();
+				JSONArray taskContributions=new JSONArray();
 				for (int j=0;j<weeks.size()-1;j++){
 					if(tasks.getJSONObject(i).getString("start-date").length()==8
 					&& tasks.getJSONObject(i).getString("due-date").length()==8){
-						taskContributions.add(getWeekPorportionInHours(
+						
+						taskContributions.put(getWeekPorportionInHours(
 								weeks.get(j),
 								convertTeamworkDateToCalendar(tasks.getJSONObject(i).getString("start-date")),
 								convertTeamworkDateToCalendar(tasks.getJSONObject(i).getString("due-date")),
@@ -113,15 +146,15 @@ public class WeeklyCharts {
 								));
 						}
 					else{
-						taskContributions.add(0.0);
+						taskContributions.put(0.0);
 					}
 				}
 				tasks.getJSONObject(i).append(weekKey, taskContributions);
 				
-				System.out.println("Added: "+taskContributions.toString()+ " to task id: "+ tasks.getJSONObject(i).getInt("id")+" with Task Name "+tasks.getJSONObject(i).getString("content"));
+			//	System.out.println("Added: "+taskContributions.toString()+ " to task id: "+ tasks.getJSONObject(i).getInt("id")+" with Task Name "+tasks.getJSONObject(i).getString("content"));
 				
 			}
-			
+			calculateTotals();
 		}
 	}
 	
@@ -193,6 +226,7 @@ public class WeeklyCharts {
 		    
 			System.out.println("Retrieved New Data");
 			addTimeCalculationsToTasks(numOfWeeks);
+			setProjectList();
 			dataIsLoaded=true;
 			
 		} catch (Exception e) {
@@ -202,6 +236,20 @@ public class WeeklyCharts {
 		
 		
 
+	}
+	
+	private void setProjectList(){
+		projects= new HashSet<String>();
+		for(int i=0;i<tasks.length();i++){
+			try {
+				projects.add(tasks.getJSONObject(i).getString("project-name"));
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		
 	}
 	
 	public void getOldData(int numOfWeeks) throws JSONException{
@@ -225,6 +273,7 @@ public class WeeklyCharts {
 			reader.close();
 			System.out.println("Retrieved Old Data");
 			addTimeCalculationsToTasks(numOfWeeks);
+			setProjectList();
 			dataIsLoaded=true;
 		}catch (Exception e) {
 			
@@ -233,17 +282,104 @@ public class WeeklyCharts {
 		
 	}
 	
-	private CategoryDataset createDataset( ) {
-	      final String fiat = "FIAT";        
-	      final String audi = "AUDI";        
-	      final String ford = "FORD";        
-	      final String speed = "Speed";        
-	      final String millage = "Millage";        
-	      final String userrating = "User Rating";        
-	      final String safety = "safety";        
-	      final DefaultCategoryDataset dataset = 
-	      new DefaultCategoryDataset( );  
-	      
+	
+	private void calculateTotals(){
+		Totals=new HashMap<String, Double>();
+		
+		//Initialize all totals to zero
+		for(int j=0;j<weeks.size();j++){	
+			for(int i=0;i<tasks.length();i++){
+				try{
+				Totals.put(tasks.getJSONObject(i).getString("responsible-party-summary")+
+						   tasks.getJSONObject(i).getString("project-name")+
+						   j //j is week number
+						   , 0.0);
+				}
+				catch(JSONException e){
+		//			System.out.println("Task: "+tasks.getJSONObject(i).get("content")+" does not have is not assigned");
+				}
+			}
+		}
+		//Calculate totals
+		for(int j=0;j<weeks.size();j++){	
+			
+			for(int i=0;i<tasks.length();i++){
+				try{
+			/*	System.out.println("Key is: "+tasks.getJSONObject(i).getString("responsible-party-summary")+
+							   tasks.getJSONObject(i).getString("project-name")+
+							   j +", hours to be added: " +tasks.getJSONObject(i).getJSONArray(weekKey).getJSONArray(0).getDouble(j)
+							   + " to "+Totals.get(tasks.getJSONObject(i).getString("responsible-party-summary")+
+									   tasks.getJSONObject(i).getString("project-name")+
+									   j).doubleValue());*/
+					
+				Totals.put(tasks.getJSONObject(i).getString("responsible-party-summary")+
+						   tasks.getJSONObject(i).getString("project-name")+
+						   j //j is week number
+						   , Totals.get(tasks.getJSONObject(i).getString("responsible-party-summary")+
+						   tasks.getJSONObject(i).getString("project-name")+
+						   j)+ (tasks.getJSONObject(i).getJSONArray(weekKey).getJSONArray(0).getDouble(j)));
+				}catch(JSONException e){
+				//	System.out.println("Task: "+tasks.getJSONObject(i).get("content")+" does not have is not assigned");
+				}
+			}
+		}
+		
+	}
+	private CategoryDataset createDataset(String person ) {
+		
+		
+		
+		
+	    final String fiat = "FIAT";        
+	    final String audi = "AUDI";        
+	    final String ford = "FORD";        
+	    final String speed = "Speed";        
+	    final String millage = "Millage";        
+	    final String userrating = "User Rating";        
+	    final String safety = "safety";        
+	    final DefaultCategoryDataset dataset = new DefaultCategoryDataset( );  
+	    
+	    Object[] projectlist= projects.toArray();
+		
+		System.out.println("Size of Project list: "+projectlist.length);
+		for (int i=0;i<projectlist.length;i++){
+			for(int j=0;j<weeks.size();j++)
+			try{
+				if(Totals.get(person+(String)projectlist[i]+j)!=0){
+						Calendar endWeek=(Calendar) weeks.get(j+1).clone();
+						endWeek.add(Calendar.DAY_OF_MONTH, -1);
+						dataset.addValue(Totals.get(person+(String)projectlist[i]+j), 
+								(String)projectlist[i], 
+								sdf.format(weeks.get(j).getTime())+" - "+sdf.format(endWeek.getTime()));
+					}
+				
+				}catch(Exception e){
+				
+			}
+		}
+		
+	    
+	    
+	    /*
+	    for (int i=0;i<tasks.length();i++){
+	    	  try {
+	    		  for(int j=0;j<weeks.size()-1;j++){
+	    			  if(person.compareTo(tasks.getJSONObject(i).getString("responsible-party-summary"))==0){
+	    				  Calendar nextWeek=(Calendar) weeks.get(j+1).clone();
+	    				  nextWeek.add(Calendar.DAY_OF_MONTH, -1);
+	    				  
+	    				  dataset.addValue(tasks.getJSONObject(i).getJSONArray(weekKey).getJSONArray(0).getDouble(j),
+	    						  tasks.getJSONObject(i).getString("project-name"), 
+	    						  sdf.format(weeks.get(j).getTime())+" - "+sdf.format(nextWeek.getTime()));
+	    				  ;
+	    			  }
+	    		  }
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				
+			}
+	    	 // dataset.addValue(value, rowKey, columnKey);weeks.get(i)
+	      }
 	      
 	      dataset.addValue( 1.0 , fiat , speed );        
 	      dataset.addValue( 3.0 , fiat , userrating );        
@@ -254,31 +390,34 @@ public class WeeklyCharts {
 	      dataset.addValue( 6.0 , audi , userrating );       
 	      dataset.addValue( 10.0 , audi , millage );        
 	      dataset.addValue( 4.0 , audi , safety );
-
+	      dataset.addValue( 20.0 , "TestCar" , safety );
 	      dataset.addValue( 4.0 , ford , speed );        
 	      dataset.addValue( 2.0 , ford , userrating );        
 	      dataset.addValue( 3.0 , ford , millage );        
-	      dataset.addValue( 6.0 , ford , safety );               
-
+	      dataset.addValue( 6.0 , ford , safety );              
+*/
 	      return dataset; 
 	   }
 	
-	public void showChart(){
+	public void showChart(String person){
 
 	
 		if(dataIsLoaded){
-			 JFreeChart barChart = ChartFactory.createBarChart(
-			         "Test",           
-			         "Category",            
-			         "Score",            
-			         this.createDataset(),          
+			
+			 JFreeChart barChart;
+			barChart = ChartFactory.createBarChart(
+			         person,           
+			         "Weeks/Project",            
+			         "Hours",            
+			         this.createDataset(person),          
 			         PlotOrientation.VERTICAL,           
 			         true, true, false);
-			
-				// create and display a frame...
 			ChartFrame frame = new ChartFrame("test",barChart);
 			frame.pack();
 			frame.setVisible(true);
+			
+				// create and display a frame...
+			
 			}
 		else{System.out.println("Data is not loaded");}
 		}
